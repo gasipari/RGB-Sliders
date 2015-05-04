bbeb1b10213a781740f821c7515196887ceadcec
Marius Muk <riamarius@gmail.com>
2015-05-04 15:32:55 -0400

    Added reg/green/blue sliders which update the view s background color

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +27,13 @@ class ViewController: UIViewController {
     }
     
     // this is a comment
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 
 
 }
